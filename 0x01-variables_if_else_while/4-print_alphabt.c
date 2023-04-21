b6c3b36e753a6e252bb9bbc36b550f091b426f29
AFAAF19 <afaf.mourky@gmail.com>
2023-04-21 16:43:43 -0700

    alphabet lower case with excep

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	alph = 'a';
+while (alph <= 'z' && alph != 'q' && alph != 'e')
+{
+putchar(alph);
+alph++;
+}
+putchar('\n');
+return (0);
+}
