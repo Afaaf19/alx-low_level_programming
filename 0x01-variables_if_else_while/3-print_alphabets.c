bc59627b1958997ab2b8a8744646d33c54f68e29
AFAAF19 <afaf.mourky@gmail.com>
2023-04-21 16:22:13 -0700

    alphabets lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph, ALPH;
+
+	alph = 'a';
+	ALPH = 'A';
+while (alph <= 'z')
+{
+putchar(alph);
+alph++;
+}
+while (ALPH <= 'Z')
+{
+putchar(ALPH);
+ALPH++;
+}
+putchar('\n');
+return (0);
+}
