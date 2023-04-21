98a1e60e59ce93997e4c1f07b5223d2ab13a7850
AFAAF19 <afaf.mourky@gmail.com>
2023-04-20 19:30:48 -0700

    write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+char chartab[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(1,chartab,59);
+return (1);
+}
