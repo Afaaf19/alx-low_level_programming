68d32414b603ffd8533bf79da81ee5b78bae2fdf
AFAAF19 <afaf.mourky@gmail.com>
2023-04-21 15:21:34 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
 else
 {
-printf("Last digit of %d is %d and is is 0\n", n, lastdigit);
+printf("Last digit of %d is 0 and is 0\n", n);
 }
 	return (0);
 }
