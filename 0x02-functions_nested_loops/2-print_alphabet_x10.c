b079d9fb81898958d2bcd19f880497f3fb4faa3a
AFAAF19 <afaf.mourky@gmail.com>
2023-04-25 13:05:34 -0700

    print alph x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - alphabets 10 times
+ *
+ * Return : 0 SUCCESS
+ */
+void print_alphabet_x10(void)
+{
+	int i, alphabet;
+
+	alphabet = 97;
+	for (i = 0; i <= 10; i++)
+	{
+		while (alphabet <= 122)
+		{
+			_putchar(alphabet);
+			alphabet++;
+			_putchar('\n');
+		}
+	}
+}
