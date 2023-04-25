952ecf742c7cf7a8f8a6fbfea47ac8239b016d11
AFAAF19 <afaf.mourky@gmail.com>
2023-04-25 16:17:37 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,17 @@
 /**
  * print_last_digit - function that prints the last digit on a number
  * @n: input variable
- * Return value of last digit
+ * Return: value of last digit
  */
 int print_last_digit(int n)
 {
 	int val;
 
 	val = n % 10;
+	if (val < 0)
+	{
+		val = val * -1;
+	}
+	_putchar (val + '0');
 	return (val);
 }
