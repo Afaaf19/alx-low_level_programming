44c8f99454d4ab84690e273f0de56a661a2a4961
AFAAF19 <afaf.mourky@gmail.com>
2023-04-27 15:05:44 -0700

    multipl ints

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: input 1
+ * @b: input 2
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+	int mult = 0;
+
+	mult = a * b;
+
+	return (mult);
+}
