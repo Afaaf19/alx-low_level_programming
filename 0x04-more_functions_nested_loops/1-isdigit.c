e1eb4e33d63c7278552b262e4bf18893758824c1
AFAAF19 <afaf.mourky@gmail.com>
2023-04-27 14:04:20 -0700

    added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isdigit - Checks for digit character
+* CODE ASCII is used in this function
+* @c: The variable to test with
+*
+* Return: 1 for digit character, 0 for anything else
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
