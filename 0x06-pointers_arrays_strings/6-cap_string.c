7564bfa4ce14335d7f24b30103fe0bb45c9d1009
AFAAF19 <afaf.mourky@gmail.com>
2023-05-04 15:04:09 -0700

    added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @stcpr: parameter
+ * Return: strcp
+ */
+char *cap_string(char *strcp)
+{
+	int i = 0;
+
+	while (strcp[i])
+	{
+		while (!(strcp[i] >= 'a' && strcp[i] <= 'z'))
+			i++;
+		if (strcp[i - 1] == ' ' ||
+			strcp[i - 1] == '\t' ||
+			strcp[i - 1] == '\n' ||
+			strcp[i - 1] == ',' ||
+			strcp[i - 1] == ';' ||
+			strcp[i - 1] == '.' ||
+			strcp[i - 1] == '!' ||
+			strcp[i - 1] == '?' ||
+			strcp[i - 1] == '"' ||
+			strcp[i - 1] == '(' ||
+			strcp[i - 1] == ')' ||
+			strcp[i - 1] == '{' ||
+			strcp[i - 1] == '}' ||
+			i == 0)
+		{
+		strcp[i] -= 32;
+		}
+		i++;
+	}
+	return (strcp);
+}
