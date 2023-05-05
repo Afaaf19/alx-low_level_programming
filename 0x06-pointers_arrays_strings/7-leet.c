4abfbeab46d0a3dfd379506cc60fa17f264998c1
AFAAF19 <afaf.mourky@gmail.com>
2023-05-05 05:50:59 -0700

    added

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337
+ * @str: parameter
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int i = 0, j = 0;
+	char s1[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char s2[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	while (str[i])
+	{
+		while (j < 10)
+		{
+			if (str[i] == s2[j])
+			{
+				str[i] = s1[j];
+			}
+		j++;
+		}
+		i++;
+	}
+	return (str);
+}
+
