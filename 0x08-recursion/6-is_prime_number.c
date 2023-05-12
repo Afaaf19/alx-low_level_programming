6c1c3db0fd0cd5e5048ba0a30cebf146fada97d2
AFAAF19 <afaf.mourky@gmail.com>
2023-05-11 19:52:44 -0700

    not yet

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - function that looks for prime numbers
+ * @n: parameter
+ * Return: 1 if it's a prime number, or 0 if else
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (prime_it(n, 2));
+}
+/**
+ * prime_it - function that checks if it prime or not
+ * @n: parameter 1
+ * @z: parameter 2
+ * Return: 1 if it's a prime number, 0 if else
+ */
+int prime_it(int n, int z)
+{
+	if (z >= n && n >= 2)
+		return (1);
+	else if (n % z == 0 || n < 2)
+		return (0);
+	else
+		return (prime_it(n, z + 1));
+}
