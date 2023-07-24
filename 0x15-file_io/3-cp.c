d1cd59f7a0afb4494a884c70e035b19e67c415f4
AFAAF19 <afaf.mourky@gmail.com>
2023-07-24 10:25:44 -0700

    added 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * cp_file - function that copies the content of a file to another file
+ * @fd_from: file to copy from
+ * @fd_to: file to copy to
+ * Return: void
+ */
+void cp_file(const char *fd_from, const char *fd_to)
+{
+	int file_from = open(fd_from, O_RDONLY);
+	int file_to = open(fd_to, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	ssize_t nb_read;
+	char buff[BUF_SIZE];
+	mode_t md = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fd_from);
+		exit(98);
+	}
+
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fd_to);
+		exit(99);
+	}
+
+	while ((nb_read = read(file_from, bff, BUF_SIZE)) > 0)
+	{
+		if (write(file_to, buff, nb_read) != nb_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fd_to);
+			exit(99);
+		}
+	}
+	if (nb_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fd_from);
+		exit(98);
+	}
+
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", file_from);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+}
+
+int main(int argc, char argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+	cp_file(argv[1], argv[2]);
+
+	return (0);
+}
