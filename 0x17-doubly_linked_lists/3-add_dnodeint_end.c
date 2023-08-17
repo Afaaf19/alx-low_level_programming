87523c2489875cee55e881370ad7707aecf35278
AFAAF19 <afaf.mourky@gmail.com>
2023-08-17 11:35:12 -0700

    modif 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,6 +14,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!head)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	curr = *head;
